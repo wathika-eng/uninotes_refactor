ff64be4121d4bfc4bf6c029adb7252e95c51214b
wathika-eng <wathika02@gmail.com>
2025-04-12 22:57:05 +0300

    Comment out OTP and debug toolbar middleware for improved configuration management

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django_otp.middleware.OTPMiddleware",
+    # "django_otp.middleware.OTPMiddleware",
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
